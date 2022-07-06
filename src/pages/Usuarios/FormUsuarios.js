79fcd8399e4f95a583e7a3366c2693fcf39c95d0
Márcio Ferreira <profhaeliomarcio@gmail.com>
2022-07-06 19:10:37 -0300

    ajustes

--- src/pages/Usuarios/FormUsuarios.js
+++ src/pages/Usuarios/FormUsuarios.js
@@ -1,12 +1,34 @@
 import { useState } from 'react';
 function FormUsuarios(){
-    const [nome, setNome] = useState('Teste');
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function submit() {
+        const data = {
+            nome: nome,
+            email: email,
+            senha: password
+        };
+        fetch("http://localhost:3001/usuarios",{
+            method: 'POST',
+            body: data
+        })
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+
+        })
+        .catch(function(error) {
+            alert(error);
+        });
+    }
 
     return (
         <div>
             <h3>Novo Usuário</h3>
-            <form>
-                {nome}
+            <form onSubmit={submit}>
                 <br />
                 <label>Nome</label>
                 <input 
@@ -17,10 +39,20 @@ function FormUsuarios(){
                 />
                 <br />
                 <label>Email</label>
-                <input type="email" name="email" />
+                <input 
+                    type="email" 
+                    name="email" 
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
                 <br />
                 <label>Senha</label>
-                <input type="password" name="senha" />
+                <input 
+                    type="password" 
+                    name="senha" 
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
                 <br />
                 <input type="submit" value="Enviar" />
             </form>
